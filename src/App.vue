195267dd69875fc78ab4f93a84fc95be6478c276
Triple Kay <triplekay.k3@gmail.com>
2021-12-21 16:03:08 +0630

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-4">
+            <div class="my-5">
+                <Title title="Vue Todo List"></Title>
+
+                <ListCreate @create="create"></ListCreate>
+
+                <div class="d-flex justify-content-between">
+                    <p class="mb-0 font-weight-bold">Job List{{ lists.length > 1 ? "s" : "" }}</p>
+                    <p v-if="lists.length > 0 && doneTotal === lists.length" class="badge badge-success badge-pill">
+                        All Done <i class="fas fa-check-circle"></i>
+                    </p>
+                    <p v-else class="badge badge-primary badge-pill">
+                        Done {{ doneTotal }}
+                    </p>
+                </div>
+
+                <ul class="list-group">
+                    <li v-if="lists.length === 0" class="list-group-item text-center">
+                      There is no job !
+                    </li>
+                    <List v-for="list in lists" :key="list.id" :list="list" @del="del"></List>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+import List from './components/List.vue';
+import ListCreate from './components/ListCreate.vue';
+import Title from './components/Title.vue';
+  export default {
+  components: { Title, List, ListCreate },
+    name : "App",
+     data() {
+       return {
+         currentId : 0,
+        
+        lists : [
+           
+        ]
+       }
+     },
+    computed : {
+        doneTotal(){
+            return this.lists.filter(el => el.isDone === true).length;
+        }
+    },
+    methods : {
+        create(x){
+            this.currentId++
+            this.lists.push({
+                id : this.currentId,
+                message : x,
+                isDone : false,
+                
+            });
+            
+        },
+        del(x){
+          // if(confirm('Are you sure to delete ?')){
+            setTimeout(()=>this.lists = this.lists.filter(el => el.id !== x ),600)
+          // }
+        }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+   .done{
+            text-decoration: line-through !important;
+            animation: 1s shakeX;
+        }
+        .created{
+            animation: 0.5s fadeInDown;
+        }
+        .deleted{
+            animation: 0.5s zoomOut;
+        }
 </style>
---
+++ src/components/ListCreate.vue
@@ -0,0 +1,44 @@
+<template>
+     <div class="form-row mb-3">
+        <div class="col-10">
+            <input type="text" class="form-control form-control-lg" v-model="newMessage"  @keyup.enter="create()" @keyup="showError=false" placeholder="Say Something">
+        </div>
+        <div class="col-2">
+            <button class="btn btn-primary btn-lg w-100" @click="create()">
+                <i class="fas fa-plus-circle"></i>
+            </button>
+        </div>
+        <div class="col-12">
+            <small v-if="showError" class="text-danger font-weight-bold">{{ errorMessage }}</small>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ListCreate",
+        data() {
+            return {
+                newMessage : "",
+                showError : false,
+                errorMessage : ""
+            }
+        },
+        methods: {
+            create() {
+                if(this.newMessage.length){
+                    this.showError = false;
+                    this.$emit("create",this.newMessage);
+                    this.newMessage = "";
+                }else{
+                    this.showError = true;
+                    this.errorMessage = "Say Something.."
+                }
+            }
+        },
+    }
+</script>
+
+<style  scoped>
+
+</style>
